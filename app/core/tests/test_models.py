d7c84e2c2cf7ef163629613b612eeec69bed78cb
sweall1 <bardziszewski@wp.pl>
2021-02-07 18:50:04 +0100

    Updated admin to  support custom user model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,19 +6,16 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
+        email = 'test1@gmail.com'
         password = 'Password123'
-        user = get_user_model().objects.create_user(
-		    email=email,
-		    password=password
-		)
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@GMAIL.COM'
+        email = 'test1@GMAIL.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
@@ -31,7 +28,7 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@gmail.com',
+            'test1@gmail.com',
             'test123'
         )
 
